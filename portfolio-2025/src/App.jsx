7b5e56206d48df2596654bea3ecfde435f0d5395
eddiejab <1jabraoutie@gmail.com>
2025-04-21 19:26:23 -0400

    comment

---
+++ portfolio-2025/src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div> 
+      <header> 
+        <h2>Eddie Jabraouti | Portfolio</h2>
+        <nav>
+          <a href='#'> Home |</a>
+          <a href='#'> About |</a>
+          <a href='#' > Projects |</a>
+          <a href='#'> Contact |</a>
+        </nav>
+        <button type='button'> Dark Mode/Light Mode </button>
+      </header>
+      <div>
+      <div>
+      <section>
+      <br />
+        <img src='' alt='Head Shot'>
+        </img>
+        <h1>Hi, Im Eddie Jabraouti</h1> 
+        <small>Aspiring ML Engineer/Data Scientist | Student At the University of Guelph </small>
+        <button> Get In Touch </button> <button> View Projects </button>
+      </section>
+      </div>
+      <br />
+        <small> <img src='' alt='small sparkle'></img> About me </small>
+        <div> 
+          <h1> Honours Computer Science Student </h1>
+          <small>Specializing in Machine Learning and Data Science & creating purpose driven Projects/AI Models </small>
+        </div>
+        <h3> <img src='' alt='grad hat'></img> Background </h3>
+        <p> I'm an Honours Computer Science student at the University of Guelph with a strong interest in Machine Learning and Data Science. With several years of programming experience in Python, Java, C, and full-stack web development, I enjoy building solutions that blend analytical thinking with real-world impact. I hope to specialize in the development of cutting-edge AI/ML Solutions that serve value. </p>
+
+      
+      <div> 
+
+      </div>
+
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
